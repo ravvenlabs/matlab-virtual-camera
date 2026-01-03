433f5a677444b4d2ab1d7dac3dddc754724b3f15
drkaputa <dskiee@rit.edu>
2026-01-03 09:25:56 -0500

    updated helloWorld.m code to do a proper loopback.

--- zynq7000/helloWorld.m
+++ zynq7000/helloWorld.m
@@ -1,16 +1,16 @@
 % Dr. Kaputa
 % Virtual Camera Demo
 % must run helloWorld.py first on the FPGA SoC
+% make sure to modify the IP address below accordingly
 
 %Initialization Parameters
-server_ip   = '182.168.1.51';     % IP address of the server
-server_port = 9999;                % Server Port of the sever
+server_ip   = '192.168.1.51';     % IP address of the server
+server_port = 9999;               % Server Port of the sever
 
 client = tcpclient(server_ip,server_port);
 fprintf(1,"Connected to server\n");
  
-write(client,'0');
+write(client,uint8(5));
 flush(client);
-
-write(client,'1');
-flush(client);
+result = read(client,1);
+disp(result)
